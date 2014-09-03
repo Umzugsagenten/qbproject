bcf12c489ebc946da40548204a51481e9c470310
Ulf Thomas <ulf.thomas@m-cube.de>
2014-09-03 16:18:23 +0200

    wip

--- qbschema/src/test/scala/org/qbproject/schema/json/ValidationBenchmark.scala
+++ qbschema/src/test/scala/org/qbproject/schema/json/ValidationBenchmark.scala
@@ -6,6 +6,8 @@ import org.scalameter.api._
 import play.api.libs.json.Json._
 import play.api.libs.json._
 
+import scala.util.Random
+
 
 object ValidationBenchmark extends PerformanceTest {
 
@@ -19,24 +21,24 @@ object ValidationBenchmark extends PerformanceTest {
   lazy val persistor = Persistor.None
 
 
-  val depths = Gen.range("depth")(1, 5, 1)
-  val fieldsPerObjects = Gen.range("fieldsPerObject")(0, 30, 5)
+  val numberOfInstancesGen = Gen.range("numberOfInstances")(500, 5000, 500)
 
-  val schemasAndInstances = for {
-    depth <- depths
-    fieldsPerObject <- fieldsPerObjects
-  } yield generate(depth, fieldsPerObject)
+  val instances = for {
+    numberOfInstances <- numberOfInstancesGen
+  } yield BenchmarkInstanceGenerator.generate(numberOfInstances)
 
   performance of "QBValidator" in {
     measure method "validate" in {
-      using(schemasAndInstances) in { t =>
-
-      val result = QBValidator.validate(t._1)(t._2)
-        if (result.isInstanceOf[JsError]) {
-          println(result)
-        }
-        result
+      using(instances) in { t =>
+        validatorRun(t)
       }
+    }
+  }
+
+  def validatorRun(objs: List[JsObject]) = {
+    objs.map {
+      instance =>
+        QBValidator.validate(BenchmarkSchema.auto)(instance)
     }
   }
 
@@ -67,7 +69,76 @@ object ValidationBenchmark extends PerformanceTest {
 
 }
 
-object BenchmarkInstanceGenerator
+object BenchmarkInstanceGenerator {
+
+  import play.api.libs.json.Json._
+
+  val r = new Random(12345)
+
+  def generate(): JsObject = {
+    obj(
+      "meta" -> obj(
+        "name" -> generateString(10),
+        "make" -> generateString(15),
+        "year" -> generateNumberString(4),
+        "price" -> r.nextInt()
+      ),
+      "extra" -> arr(
+        obj(
+          "name" -> generateString(10),
+          "description" -> generateString(1000),
+          "price" -> r.nextInt()
+        )
+      ),
+      "tires" -> arr(
+        obj(
+          "diameter" -> r.nextInt(30),
+          "width" -> r.nextInt(20),
+          "color" -> "red",
+          "material" -> generateString(10)
+        )
+      ),
+      "technicalData" -> obj(
+        "engine" -> obj(
+          "capacity" -> r.nextInt(),
+          "torque" -> r.nextInt(),
+          "power" -> r.nextInt()
+        ),
+        "maxVelocity" -> r.nextInt(),
+        "weight" -> r.nextInt()
+      ),
+      "interior" -> obj(
+        "colorOne" -> generateString(10),
+        "colorTwo" -> generateString(10),
+        "colorThree" -> generateString(10)
+      ),
+      "exterior" -> obj(
+        "colorOne" -> generateString(10),
+        "colorTwo" -> generateString(10)
+      )
+    )
+  }
+
+  def generate(cnt: Int): List[JsObject] = {
+    (0 until cnt).map {
+      idx =>
+        generate()
+    }.toList
+  }
+
+  def generateString(cnt: Int): String = {
+    r.nextString(cnt)
+  }
+
+  def generateNumberString(cnt: Int): String = {
+    val builder = new StringBuilder()
+    for (i <- 0 until cnt) {
+      builder.append(r.nextInt(10))
+    }
+    builder.toString()
+  }
+
+}
 
 object BenchmarkSchema {
 
@@ -111,7 +182,26 @@ object BenchmarkSchema {
     "exterior" -> qbClass(
       "colorOne" -> color,
       "colorTwo" -> color
-    )
+    ),
+    "objects" -> createObject(2, 10)
   )
+
+  def createObject(depth: Int, width: Int): QBClass = {
+    if (depth <= 0) {
+      qbClass(
+        Range(0, width).map {
+          idx =>
+            idx.toString -> qbString
+        }: _ *
+      )
+    } else {
+      qbClass(
+        Range(0, width).map {
+          idx =>
+            idx.toString -> createObject(depth - 1, width)
+        }: _ *
+      )
+    }
+  }
 
 }
